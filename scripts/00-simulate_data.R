78be5626cb26db2a0ee70705a4f67bcfea522769
TEJMaster <69652431+TEJMaster@users.noreply.github.com>
2024-03-26 18:50:00 -0400

    updated simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -15,5 +15,11 @@ library(tidyverse)
 data <- 
   tibble(
     level = rnorm(n = 1000, mean = 100, sd = 10) |> floor(),
-    weakness = sample(x = c("fire", "not fire"), size = 1000, replace = TRUE)
+    weakness = sample(x = c("fire", "not fire"), size = 1000, replace = TRUE),
+    type = sample(x = c("water", "fire", "grass", "electric", "psychic"), size = 1000, replace = TRUE),
+    attack = rnorm(n = 1000, mean = 75, sd = 15) |> floor(),
+    defense = rnorm(n = 1000, mean = 70, sd = 15) |> floor(),
+    speed = rnorm(n = 1000, mean = 65, sd = 15) |> floor()
   )
+
+head(data)
